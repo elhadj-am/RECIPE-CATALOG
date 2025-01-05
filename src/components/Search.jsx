6ea9fd8fd6d5b20b570ce55f41ad10124494daf9
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-01-05 15:46:20 +0100

    Making API AND Getting data  (#2)
    * RECIPE-CATALOGUE WIP
    
    * Update Search.jsx
    
    * Making API  and Getting data

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "5ab6ac5496d647708f62c8200e307560";
+
 export default function () {
   const [query, setQuery] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      // console.log("useEffect Called");
+      const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchData();
+  }, [query]);
   return (
     <div>
-      <input value={query} type="text" />
-      <input value={query} type="text" />
-
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
     </div>
   );
 }
