7cb90054015ab9684f3b53451f204c4dc64aa81d
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-01-07 13:44:56 +0100

    update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,14 +3,17 @@ import { useEffect, useState } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "5ab6ac5496d647708f62c8200e307560";
 
-export default function () {
+export default function ({ foodData, setFoodData }) {
   const [query, setQuery] = useState("");
+
   useEffect(() => {
     async function fetchData() {
       // console.log("useEffect Called");
       const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await response.json();
-      console.log(data);
+
+      console.log(data.results);
+      setFoodData(data.results);
     }
     fetchData();
   }, [query]);
