cea87e7c1d5662d5519cfacf464c0193797cb7aa
Elhadj <46904847+elhadj-am@users.noreply.github.com>
2025-01-26 17:31:28 +0100

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,6 +5,7 @@ export default function Item({ item }) {
       <div className={styles.itemContainer} key={item.name}>
         <div className={styles.imageContainer}>
           <img
+            className={styles.image}
             src={
               `https://spoonacular.com/cdn/ingredients_100x100/` + item.image
             }
